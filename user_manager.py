576be19817ca4715d66e49d92d7b04691ab91cdf
ghazaale <qazale02@gmail.com>
2024-05-17 12:13:32 +0330

    change methods args name

--- user_manager.py
+++ user_manager.py
@@ -8,15 +8,15 @@ class UserManager(ModelManager):
         # ModelManager.table_name = table_name
         # ModelManager.model_class = model_class
         
-    def login_user(self, username, password):
-        user_obj = self.get('username', username)
+    def login_user(self, entered_username, entered_password):
+        user_obj = self.get('username', entered_username)
         if user_obj:
-            if user_obj.check_password(password):
+            if user_obj.check_password(entered_password):
                 return user_obj
         print("no username ")
         # raise ValueError("Invalid username or password")
 
-    def register_user(self, username, password):
+    def register_user(self, entered_username, entered_password):
         # get user by username
         # q = "select * from users where username = %s"
         # with self.db_manager as db:
@@ -26,15 +26,15 @@ class UserManager(ModelManager):
         #         print(" user exists in database")
         #         return
             
-        res = self.get('username', username)
+        res = self.get('username', entered_username)
         if res :
             raise ValueError(" Username already in database")
         # create new user
-        # hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        # hashed_password = hashlib.sha256(entered_password.encode()).hexdigest()
         # new_user = User(username, hashed_password) 
         
-        new_user = User(username, hashed_password) 
-        new_user.password = password ### class setter method ( call hash method)
+        new_user = User(entered_username) 
+        new_user.password = entered_password ### class setter method ( call hash method)
         # password : plain text --> _password
         # new_user = User(username) # password : plain text --> _password
         # new_user.password = password # call setter and hash password
