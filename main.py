15d8ab021137f74a5a40610ce987eb802c34eed5
ghazaale <qazale02@gmail.com>
2024-05-17 12:09:15 +0330

    implement login and register in cli

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 from database_manager import DatabaseManager
 from model_manager import ModelManager
+from user_manager import UserManager
 from models import User
 
 import hashlib
 
-
+with open("p.txt", "r") as f:
+    password = f.read()
+    
 db_config = {
     'dbname': 'm_110',
-    'user' : 'maktab_user',
-    'password' : '123',
+    'user' : 'postgres',
+    'password' : password,
     'host' : 'localhost'
 }
 
 db_manager_obj = DatabaseManager(db_config)
-user_model_manager_obj = ModelManager(db_manager_obj, 'users', User)
-users_obj = user_model_manager_obj.all()
-for user in users_obj:
-    user[0], user[1]
 
-user_model_manager_obj.get(id)
-user_model_manager_obj.filter(column, values)
+# User.manager = ModelManager(db_manager_obj, 'users', User) # old approach
+# Contact.manager = ModelManager(db_manager_obj, 'Contacts', Contact) # old approach
+### we need login and regoster 
+# User.manager = UserManager(db_manager_obj, 'users', User) # X Wrong
+User.manager = UserManager(db_manager_obj) # Correct
+# show all users
+# User.manager.all()
+# for user in users_obj:
+#     user[0], user[1]
+
+# user_model_manager_obj.get(id)
+# user_model_manager_obj.filter(column, values)
 
 # show menu
 def display_menu():
@@ -33,13 +42,22 @@ def display_menu():
 def register_user():
     username = input("Enter your username: ")
     password = input("Enter your password: ")
+    new_user = User.manager.register_user(username, password)
+    if new_user:
+        print(" registration successful --------------------------------")
+    
     
     
 # login 
 def login():
     username = input("Enter your username: ")
     password = input("Enter your password: ")
-    
+    logged_user = User.manager.login_user(username, password)
+    if not logged_user:
+        print("login failed --------------------------------")
+        return
+    current_user_id  = logged_user.user_id
+    print("login successful --------------------------------")
    
 
 def main():
