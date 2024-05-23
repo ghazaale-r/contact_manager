c1074c6bbaccc37344fa5f97ac8a4aef729564d5
ghazaale <qazale02@gmail.com>
2024-05-23 21:15:04 +0330

    implement contact menu and actions (add/edit/del)

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from database_manager import DatabaseManager
 from model_manager import ModelManager
 from user_manager import UserManager
-from models import User
+from contact_manager import ContactManager
+from models import User,Contact
 
 import hashlib
 
@@ -22,6 +23,7 @@ db_manager_obj = DatabaseManager(db_config)
 ### we need login and regoster 
 # User.manager = UserManager(db_manager_obj, 'users', User) # X Wrong
 User.manager = UserManager(db_manager_obj) # Correct
+Contact.manager = ContactManager(db_manager_obj)
 # show all users
 # User.manager.all()
 # for user in users_obj:
@@ -32,10 +34,55 @@ User.manager = UserManager(db_manager_obj) # Correct
 
 # show menu
 def display_menu():
+    print("\n===== Main Menu =====")
     print("1. Register new user")
     print("2. Login as user")
     print("3. Exit")
     return input("Enter your Choice: ")
+
+def  display_contact_menu() -> int:
+    print("\n===== Contact Menu =====")
+    print("1. add contact")
+    print("2. edit contact")
+    print("3. delete contact")
+    print("4. back to main menu")
+    return input('Enter your choice:')
+
+def add_contact(logged_user_obj):
+    while True:
+        name =  input("Enter your name")
+        if name:
+            break
+         
+    email = input("Enter your email")
+    while not email:
+        email = input("Enter your email again")
+
+    # ContactManager.add_contact(logged_user_obj.user_id, name, email)
+    Contact.manager.add_contact(logged_user_obj.user_id, name, email)
+
+def edit_contact(logged_user_obj):
+    pass
+
+def delete_contact(logged_user_obj):
+    pass
+
+# logged menu
+def contact_main(logged_user_obj):
+    while True:
+        n = display_contact_menu()
+        if n == '1':
+            add_contact(logged_user_obj)
+        elif n == '2':
+            edit_contact(logged_user_obj)
+        elif n == '3':
+            delete_contact(logged_user_obj)
+        elif n == '4':
+            print("backing to main menu...")
+            break
+        else:
+            print("invalid input")
+
 
 
 # register
@@ -52,13 +99,12 @@ def register_user():
 def login():
     username = input("Enter your username: ")
     password = input("Enter your password: ")
-    logged_user = User.manager.login_user(username, password)
-    if not logged_user:
+    logged_user_obj = User.manager.login_user(username, password)
+    if not logged_user_obj:
         print("login failed --------------------------------")
         return
-    current_user_id  = logged_user.user_id
     print("login successful --------------------------------")
-   
+    contact_main(logged_user_obj)
 
 def main():
     while True:
